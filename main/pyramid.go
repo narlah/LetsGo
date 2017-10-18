79c047a44f831fd616320e9b75b054fa3e818766
narlah <mitrandir@gmail.com>
2017-10-18 22:28:35 +0300

    nothing

---
+++ main/pyramid.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	number := 20;
+	for i := 0; i <= number; i++ {
+		if i==20 {
+			fmt.Println(repeatEmpty(number-i) + "#x" +  repeatEmpty(i-1)+ "|z"+ repeatEmpty(i-1) + "#y")
+			break
+		}
+		fmt.Println(repeatEmpty(number-i) + "#" +  repeatEmpty(i)+ "|"+ repeatEmpty(i) + "#")
+
+	}
+
+	for i := 0; i <= number; i++ {
+		fmt.Println(strings.Repeat(" ", number-i) + strings.Repeat("#", i*2))
+	}
+
+}
+
+func repeatEmpty(i int) string{
+	return strings.Repeat(" ", i)
+}
