336d2b5d6240cf3b2b4e668813ec7b93ac19ec58
narlah <mitrandir@gmail.com>
2018-03-06 10:39:27 +0200

    problem10 , sum of primes to 2mil

---
+++ main/euler/problem10.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"math"
+	m "math/big"
+	"fmt"
+)
+
+const N = 2000000
+
+func main() {
+	var x, y, n int
+	nsqrt := math.Sqrt(N)
+
+	is_prime := [N]bool{}
+
+	for x = 1; float64(x) <= nsqrt; x++ {
+		for y = 1; float64(y) <= nsqrt; y++ {
+			n = 4*(x*x) + y*y
+			if n <= N && (n%12 == 1 || n%12 == 5) {
+				is_prime[n] = !is_prime[n]
+			}
+			n = 3*(x*x) + y*y
+			if n <= N && n%12 == 7 {
+				is_prime[n] = !is_prime[n]
+			}
+			n = 3*(x*x) - y*y
+			if x > y && n <= N && n%12 == 11 {
+				is_prime[n] = !is_prime[n]
+			}
+		}
+	}
+
+	for n = 5; float64(n) <= nsqrt; n++ {
+		if is_prime[n] {
+			for y = n * n; y < N; y += n * n {
+				is_prime[y] = false
+			}
+		}
+	}
+
+	is_prime[2] = true
+	is_prime[3] = true
+
+	primes := make([]int, 0, 1270606)
+	for x = 0; x < len(is_prime)-1; x++ {
+		if is_prime[x] {
+			primes = append(primes, x)
+		}
+	}
+	bigResult := m.NewInt(0)
+	for _, i := range primes {
+		fmt.Println(i)
+		bigResult.Add(bigResult, m.NewInt(int64(i)))
+	}
+	fmt.Println(bigResult)
+}
