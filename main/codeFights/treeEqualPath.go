6682f426b860744f12b47d664a98a6cda3da7963
narlah <mitrandir@gmail.com>
2018-10-07 17:22:26 +0300

    Small thing about path to leaf PS : alergy : moss , finally identified it

---
+++ main/codeFights/treeEqualPath.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+//
+// Definition for binary tree:
+type Tree struct {
+  Value interface{}
+  Left *Tree
+  Right *Tree
+}
+func hasPathWithGivenSum(t *Tree, s int) bool {
+	if t == nil {return false}
+	val := s-t.Value.(int)
+	if val == 0 && t.Left == nil && t.Right == nil { return true }
+	return hasPathWithGivenSum (t.Left, val ) || hasPathWithGivenSum (t.Right, val)
+}
+func main() {
+	t := &Tree{5, nil, nil}
+	fmt.Println(hasPathWithGivenSum(t,5))
+}
